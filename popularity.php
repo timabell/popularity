0723d5cb187f2c7795f3cbfb2280c0e176441b4c
Tim Abell <tim@timwise.co.uk>
2010-04-10 23:07:23 +0100

    use timestamp for submission filenames

--- popularity.php
+++ popularity.php
@@ -1,11 +1,14 @@
 <?php
 	$packages = $_POST['installed-package-list'];
-	$file = dirname(__FILE__)."/submissions/packages.txt";
-	print "writing to $file";
-	$fp = fopen($file, "w") or die ("couldn't open $file for writing");
+	//i know you could get 2 uploads in the same second and clobber the file, but
+	//i don't care, because it's only for a rough idea of install base :-)'
+	$name = "submissions/packages".time().".txt";
+	print "writing to $name ...";
+	$file = dirname(__FILE__)."/".$name;
+	$fp = fopen($file, "w") or die ("couldn't open $name for writing");
 	fwrite($fp, $packages) or die ("write failed");
 	fclose($fp);
-	echo "success";
+	echo "<p>success</p>";
 	print "<blockquote>";
 	print $packages;
 	print "</blockquote>";
